b18c9dfd24bea2b33aec2dfe5b20a64b930d755e
Nandor Devai <mail@nandordevai.net>
2023-10-01 23:01:39 +0200

    update presentation, simplify example code, rename bar chart

--- p5-column/sketch.js
+++ p5-column/sketch.js
@@ -1,10 +1,10 @@
 let values = null;
-const marginLeft = 40;
-const textMargin = 10;
-const rectWidth = 40;
+let marginLeft = 40;
+let textMargin = 10;
+let rectWidth = 40;
 
 function setup() {
-    values = Array.from({ length: 10 }, _ => random(10, 200));
+    values = Array.from({ length: 10 }, item => random(10, 200));
     createCanvas(windowWidth / 2, windowHeight / 2);
     colorMode(HSL);
     textAlign(CENTER);
@@ -15,9 +15,9 @@ function draw() {
     background(255);
     noStroke();
     fill(240, 40, 60);
-    values.forEach((_, i) => {
+    values.forEach((value, i) => {
         let posX = map(i, 0, values.length, marginLeft, width);
-        rect(posX, height, rectWidth, -_);
-        text(round(_), posX + (rectWidth / 2), height - _ - textMargin);
+        rect(posX, height, rectWidth, -value);
+        text(round(value), posX + (rectWidth / 2), height - value - textMargin);
     });
 }
