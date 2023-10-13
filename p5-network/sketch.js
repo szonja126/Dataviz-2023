3f40c3ad72b39b745801dac4f79b53ffa34d3230
Nandor Devai <mail@nandordevai.net>
2023-10-13 10:49:44 +0200

    cleanup network code, add gradient line

--- p5-network/sketch.js
+++ p5-network/sketch.js
@@ -3,7 +3,7 @@ import createGraph from 'ngraph.graph';
 import createLayout from 'ngraph.forcelayout';
 import { data } from './miserables';
 
-let sketch = (s) => {
+let sketch = (p) => {
     let graph;
     let layout;
     let physicsSettings = {
@@ -17,8 +17,10 @@ let sketch = (s) => {
     };
     let width = window.innerWidth;
     let height = window.innerHeight;
+    let linkEndColor = null;
+    let linkMidColor = null;
 
-    s.setup = () => {
+    p.setup = () => {
         graph = createGraph();
         data.nodes.forEach((node, i) => {
             graph.addNode(i, node);
@@ -27,39 +29,60 @@ let sketch = (s) => {
             graph.addLink(link.source, link.target, link.value);
         });
         layout = createLayout(graph, physicsSettings);
-        s.createCanvas(width, height);
-        s.colorMode(s.HSL, 360, 100, 100);
-        s.noLoop();
+        p.createCanvas(width, height);
+        p.colorMode(p.HSL, 360, 100, 100);
+        linkEndColor = p.color(240, 80, 80);
+        linkMidColor = p.color(240, 80, 50);
+        p.noLoop();
     };
 
-    s.draw = () => {
-        s.background(0, 0, 10);
+    p.draw = () => {
+        p.background(0, 0, 10);
         for (let i = 0; i < 200; i++) {
             layout.step();
         }
-        s.translate(width / 2, height / 2);
-        s.stroke(30, 50, 40);
+        p.translate(width / 2, height / 2);
         graph.forEachLink(drawLink);
-        s.fill(240, 80, 80);
-        s.stroke(0, 0, 30);
+        p.fill(240, 80, 80);
+        p.stroke(0, 0, 30);
         graph.forEachNode(drawNode);
+        p.fill(0, 0, 100);
+        graph.forEachNode(drawLabel);
     };
 
     const drawNode = (node) => {
-        console.log(node);
         const pos = layout.getNodePosition(node.id);
-        const circle = s.ellipse(pos.x, pos.y, 10, 10);
-        s.text(node.data.name, pos.x + 10, pos.y + 10);
+        const circle = p.ellipse(pos.x, pos.y, 12, 12);
+    };
+
+    const drawLabel = (node) => {
+        const pos = layout.getNodePosition(node.id);
+        p.text(node.data.name, pos.x + 10, pos.y + 10);
     };
 
     const drawLink = (link) => {
         const pos = layout.getLinkPosition(link.id);
-        s.line(
+        gradientLine(
+            p.drawingContext,
             pos.from.x,
             pos.from.y,
             pos.to.x,
-            pos.to.y
+            pos.to.y,
+            linkEndColor,
+            linkMidColor
         );
+    };
+
+    const gradientLine = (ctx, x1, y1, x2, y2, c1, c2) => {
+        const gradient = ctx.createLinearGradient(x1, y1, x2, y2);
+        gradient.addColorStop(0, c1);
+        gradient.addColorStop(.5, c2);
+        gradient.addColorStop(1, c1);
+        ctx.strokeStyle = gradient;
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
     };
 }
 
